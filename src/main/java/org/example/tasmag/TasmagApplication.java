606c0d3aee7859c420e5438394c5466f123094a8
Katarina Kovacova <katarinakovacova100@gmail.com>
2024-09-10 17:50:57 +0200

    First Commit

---
+++ src/main/java/org/example/tasmag/TasmagApplication.java
@@ -0,0 +1,13 @@
+package org.example.tasmag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasmagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasmagApplication.class, args);
+	}
+
+}
