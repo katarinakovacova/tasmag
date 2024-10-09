427fee08d37e77d3b1eb1eedf5a27ce8e3d65ff6
Katarina Kovacova <katarinakovacova100@gmail.com>
2024-10-09 12:58:14 +0200

    Test User entity #8

---
+++ src/test/java/org/example/tasmag/UserControllerTest.java
@@ -0,0 +1,101 @@
+package org.example.tasmag;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.example.tasmag.user.User;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("classpath:application-test.properties")
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final String path = "/users";
+    private final String pathWithId = "/users/{id}";
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final User user1 = new User("Balovic", "balovic@example.com", "123");
+    private final User user2 = new User("Brmbal", "brmbal@example.com", "456");
+
+    @Test
+    @Order(0)
+    void createUser() throws Exception {
+        String json1 = objectMapper.writeValueAsString(user1);
+        String json2 = objectMapper.writeValueAsString(user2);
+
+        mockMvc.perform(post(path)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(json1))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.username", is("Balovic")))
+                .andExpect(jsonPath("$.email", is("balovic@example.com")))
+                .andExpect(jsonPath("$.password", is("123")));
+
+        mockMvc.perform(post(path)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(json2))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", is(2)))
+                .andExpect(jsonPath("$.username", is("Brmbal")))
+                .andExpect(jsonPath("$.email", is("brmbal@example.com")))
+                .andExpect(jsonPath("$.password", is("456")));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void getUsers() throws Exception {
+        mockMvc.perform(get(path))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void getUserById() throws Exception {
+        mockMvc.perform(get(pathWithId, 2))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", is(2)))
+                .andExpect(jsonPath("$.username", is("Brmbal")))
+                .andExpect(jsonPath("$.email", is("brmbal@example.com")));
+
+        mockMvc.perform(get(pathWithId, 9))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void deleteUser() throws Exception {
+        mockMvc.perform(delete(pathWithId, 1))
+                .andExpect(status().isNoContent());
+
+        mockMvc.perform(get(pathWithId, 1))
+                .andExpect(status().isNotFound());
+    }
+}
